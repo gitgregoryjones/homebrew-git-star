7a27290d89d9fd6c5802eb52c94d3bbd2f4f4995
Greg Jones <gregory.jones@turner.com>
2019-07-14 23:12:30 -0400

    Updated git-star library

--- git-star.rb
+++ git-star.rb
@@ -1,9 +1,9 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/v1.0/git-star-1.0.0.tar.gz"
+  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.1/git-star-1.1.tar.gzip"
   sha256 "6943dbae10d85f42acb16cc6c93ecccbf2007f8b1cdbb2a67682f12a7b9a015b"
-  version "1.0.0"
+  version "1.1"
 
   depends_on "hub"
 
