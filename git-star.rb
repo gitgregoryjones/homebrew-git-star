81c26b1f59f95829e2d5e3f69580db9a99355766
Greg Jones <gregory.jones@turner.com>
2019-07-15 00:22:09 -0400

    Add new install:

--- git-star.rb
+++ git-star.rb
@@ -1,9 +1,9 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.3/git-star-1.3.tar.gzip"
-  sha256 "d4b7bf2412fb2ff3b01088307f1ef112ded16afbba6e3c2c52bc1f307fe6b622"
-  version "1.3"
+  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/v1.4.0/file.tar.gz"
+  sha256 "861d567b05a45f53db3e7a71078c8d136dc14fc800a2db44af754394eecfcde0"
+  version "1.4.0"
 
   depends_on "hub"
 
