36693c39047ebffc9e89da0e99955046d5e990d6
Greg Jones <gregory.jones@turner.com>
2019-07-14 23:59:02 -0400

    Fixed...wrong branch

--- git-star.rb
+++ git-star.rb
@@ -1,9 +1,9 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.2/git-star-1.2.tar.gzip"
-  sha256 "1a4922d7b51f5bde103a40789aaf337b3eb202e4028c78394c4cd9e928181f3a"
-  version "1.2"
+  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.3/git-star-1.3.tar.gzip"
+  sha256 "d4b7bf2412fb2ff3b01088307f1ef112ded16afbba6e3c2c52bc1f307fe6b622"
+  version "1.3"
 
   depends_on "hub"
 
