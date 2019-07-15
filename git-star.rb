a1ac49f565c809fdb41a570cc55145dc80a40446
Greg Jones <gregory.jones@turner.com>
2019-07-14 23:20:00 -0400

    Recalc Sha

--- git-star.rb
+++ git-star.rb
@@ -2,7 +2,7 @@ class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
   url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.1/git-star-1.1.tar.gzip"
-  sha256 "6943dbae10d85f42acb16cc6c93ecccbf2007f8b1cdbb2a67682f12a7b9a015b"
+  sha256 "74fb52628a62e9662451d735437860b01d961a86f8458c8d951ab82492963217"
   version "1.1"
 
   depends_on "hub"
