87098e8a4ce91b07ba82fcbde0fd09bba4f3df92
Greg Jones <gregory.jones@turner.com>
2019-07-14 23:33:19 -0400

    Testing

--- git-star.rb
+++ git-star.rb
@@ -3,13 +3,13 @@ class GitStar < Formula
   homepage "https://github.com/gitgregoryjones/git-star"
   url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/1.2/git-star-1.2.tar.gzip"
   sha256 "1a4922d7b51f5bde103a40789aaf337b3eb202e4028c78394c4cd9e928181f3a"
-  version "1.1"
+  version "1.2"
 
   depends_on "hub"
 
   bottle :unneeded
 
   def install
-    bin.install "git-start", "git-test", "git-release", "workflow.library"
+    bin.install "git-git-star-version", "git-start", "git-test", "git-release", "workflow.library"
   end
 end
