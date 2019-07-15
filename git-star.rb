544fc5f89d304f6c5d82111192c1befb0412216c
Greg Jones <gregory.jones@turner.com>
2019-07-14 20:50:11 -0400

    Updated download URL

--- git-star.rb
+++ git-star.rb
@@ -1,7 +1,7 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/git-star/blob/master/releases/git-star-1.0.0.tar.gz?raw=true"
+  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/v1.0/git-star-1.0.0.tar.gz"
   sha256 "6943dbae10d85f42acb16cc6c93ecccbf2007f8b1cdbb2a67682f12a7b9a015b"
   version "1.0.0"
 
