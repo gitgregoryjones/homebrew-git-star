f47ad1fd595a661800e8a1e38218194ba0fb44dc
gitgregoryjones <gregory.jones@turner.com>
2020-03-14 18:15:05 -0400

    Add new release

--- git-star.rb
+++ git-star.rb
@@ -1,8 +1,8 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/homebrew-git-star/releases/download/v1.5/files2.tar.gz"
-  sha256 "62dc77c3222918ceb25091bbcbee6d663c3937feaab61fbaaaf86943503407db"
+  url "https://github.com/gitgregoryjones/git-star/archive/v1.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "1.5.0"
 
   depends_on "hub"
