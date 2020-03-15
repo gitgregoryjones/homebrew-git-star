18531526dc68a8d5a5ef0aec85fb2f3ebc752e39
gitgregoryjones <gregory.jones@turner.com>
2020-03-14 22:04:23 -0400

    Update git-star.rb

--- git-star.rb
+++ git-star.rb
@@ -1,7 +1,7 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://github.com/gitgregoryjones/git-star/archive/v1.1.tar.gz"
+  url "https://codeload.github.com/gitgregoryjones/git-star/tar.gz/v1.1"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "1.5.0"
 
