a358b84cebd312a6d5b9ada0a7a0f9e53f19c898
gitgregoryjones <gregory.jones@turner.com>
2020-03-15 22:51:46 -0400

    Update git-star.rb

--- git-star.rb
+++ git-star.rb
@@ -3,7 +3,7 @@ class GitStar < Formula
   homepage "https://github.com/gitgregoryjones/git-star"
   url "https://codeload.github.com/gitgregoryjones/git-star/tar.gz/v4.0"
   sha256 "d79cce63f5a8257bec820eb692ba94061e0c76d67e5864a3f20a573a30269e2b"
-  version "1.5.0"
+  version "4.0"
 
   depends_on "hub"
 
