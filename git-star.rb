67d61015e95530bba1d5ff7c01fe486b6dcf07cb
gitgregoryjones <gregory.jones@turner.com>
2020-03-28 18:22:42 -0400

    Upgrade to version 4.4

--- git-star.rb
+++ git-star.rb
@@ -1,15 +1,15 @@
 class GitStar < Formula
   desc "A drop dead simple git development workflow"
   homepage "https://github.com/gitgregoryjones/git-star"
-  url "https://codeload.github.com/gitgregoryjones/git-star/tar.gz/v4.0"
-  sha256 "d79cce63f5a8257bec820eb692ba94061e0c76d67e5864a3f20a573a30269e2b"
-  version "4.0"
+  url "https://codeload.github.com/gitgregoryjones/git-star/tar.gz/v4.4"
+  sha256 "3fbb3bf5e3a109984943295048a527b75f0f2e77a5b95631d4a087f42f38ae0d"
+  version "4.4"
 
   depends_on "hub"
 
   bottle :unneeded
 
   def install
-    bin.install "git-start", "git-test", "git-staging", "git-release", "workflow.library" 
+    bin.install "git-start", "git-test", "git-acceptance", "git-release", "workflow.library" 
   end
 end
